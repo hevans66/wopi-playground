e0c0e6a178a703372329cc7d6bfdc4cd6d7c3740
Humberto Evans <hevans@circuitlab.com>
2014-05-15 23:34:33 -0700

    poop

--- wopiplay/urls.py
+++ wopiplay/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^stuff/wopi/files/(?<fileid>[^/]+)$','wopiplay.wopi.views.info'),
-    url(r'^stuff/wopi/files/(?<fileid>[^/]+)/contents$','wopiplay.wopi.views.contents'),
-    url(r'geturl/(?<fileid>[^/]+)/','wopiplay.wopi.views.get_wopi_url')
+    url(r'^stuff/wopi/files/(?P<fileid>[^/]+)$','wopiplay.wopi.views.info'),
+    url(r'^stuff/wopi/files/(?P<fileid>[^/]+)/contents$','wopiplay.wopi.views.contents'),
+    url(r'geturl/(?P<fileid>[^/]+)/','wopiplay.wopi.views.get_wopi_url')
 )
