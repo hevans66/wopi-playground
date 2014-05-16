c9d4027f1ba320bba81387e36182c5e57318fb6c
Humberto Evans <hevans@circuitlab.com>
2014-05-15 23:32:26 -0700

    poop

--- wopiplay/wopi/views.py
+++ wopiplay/wopi/views.py
@@ -5,13 +5,18 @@ import hashlib
 import json
 import base64
 
+def fileid_to_url(fileid):
+ if fileid == 'test2.docx':
+    return 'https://app.box.com/shared/static/oif4oljhrdk9zrm0f8z8.docx'
+  elif fileid == 'test4.xlsx':
+    return 'https://app.box.com/shared/static/dcrv1hljglo53fy6nvaj.xlsx'
+ 
 # Create your views here.
-def info(request):
-  f = urllib2.urlopen('https://app.box.com/shared/static/oif4oljhrdk9zrm0f8z8.docx') 
-  #f = urllib2.urlopen('https://app.box.com/shared/static/z5uvek60a8r0q1q2wpca.xlsx') 
+def info(request,fileid=None):
+  f = urllib2.urlopen(fileid_to_url(fileid)) 
   stuff = f.read()
   r = {}
-  r['BaseFileName'] = 'test.docx'
+  r['BaseFileName'] = fileid
   r['OwnerId'] = 'lovezors'
   r['Size'] = len(stuff)
   dig = hashlib.sha256(stuff).digest()
@@ -19,9 +24,12 @@ def info(request):
   r['Version'] = '1'
   return HttpResponse(json.dumps(r), content_type="application/json")
 
-def contents(request):
-  f = urllib2.urlopen('https://app.box.com/shared/static/oif4oljhrdk9zrm0f8z8.docx') 
+def contents(request,fileid=None):
+  f = urllib2.urlopen(fileid_to_url(fileid) 
   #f = urllib2.urlopen('https://app.box.com/shared/static/z5uvek60a8r0q1q2wpca.xlsx') 
   stuff = f.read()
   return HttpResponse(stuff,content_type="application/octet-stream")
 
+def get_wopi_url(request,fileid=None):
+  url = 'hi'
+  return HttpResponse(url)
